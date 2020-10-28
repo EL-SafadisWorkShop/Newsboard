01fa0f83f3b05d67c3dd75dbac436b17adb21492
lidiyacheb <lidia.saulite@gmail.com>
2020-10-28 16:12:11 +0100

    ass article section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import Article from './Article'
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    fetch("https://hn.algolia.com/api/v1/search?query=react")
+    .then((response) => response.json())
+    .then((response) => {
+        const newArticles = response.hits.map((result) => ({
+            text: result.title,
+            url: result.url,
+            author: result.author,
+            points: result.points,
+            num_comments: result.num_comments
+        }))
+      setArticles(newArticles);
+      setQuery(response.query);
+    })
+    //throw the error
+    .catch(err => alert(err));
+}, []);
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="sidebar"></div>
+      <div className="main-content">
+      {articles.map((article, index) => (
+          <Article
+            key={index}
+            index={index}
+            article={article}
+          />
+          ))}
+      </div>
+      </div>
   );
 }
 
