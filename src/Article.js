15b97d438426f2264e5e2d978e4323791d2f5675
Yazzen EL-Safadi <elsafadiyazzen@yahoo.com>
2020-10-28 17:56:19 +0100

    What ever

--- src/Article.js
+++ src/Article.js
@@ -1,9 +1,9 @@
-export default function Article({ article, index, author, points }) {
+export default function Article({ article, index, author, points, completeButton }) {
     return (
       <article className="Story">
         <div className="Story_container">
           <div className="Story_data">
-            <a href={article.url}><h1 className="Story_title">{article.text}</h1></a>
+            <a href={article.url}><h1 className="Story_title" style={{textDecoration:article.isCompleted?"line-through":""}}>{article.text}</h1></a>
             <div className="Story_meta">
               <span>{article.points} points</span>
               <span className="Story_separator"> </span>
@@ -11,7 +11,9 @@ export default function Article({ article, index, author, points }) {
               <span className="Story_separator"> </span>
               <span className="Story_separator">|</span>
               <span>{article.num_comments} comments</span>
+              <button className="completebutton" onClick={() => completeButton(index)}>Done</button>
             </div>
+            
           </div>
         </div>
       </article>
